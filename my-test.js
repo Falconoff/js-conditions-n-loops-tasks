eedc4bbe86d64e8e7daf8ecab5c8f64f4b31d4d1
Falconoff <sokolovserega1982@gmail.com>
2024-04-10 22:46:44 +0300

    sortByAsc tries

--- my-test.js
+++ my-test.js
@@ -1,23 +1,25 @@
-function isContainNumber(num, digit) {
-  const numToString = '' + num;
-  const digitToString = '' + digit;
+function sortByAsc(arr) {
+  if (arr.length < 2) return arr;
+  let pivot = arr[0];
+  const left = [];
+  const right = [];
 
-  const nts = String(num);
-
-  console.log(nts === numToString);
-  // for (let i = 0; i < numToString.length; i += 1) {
-  //   if (numToString[i] === digitToString) {
-  //     return true;
-  //   }
-  // }
-  // return false;
+  for (let i = 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return sortByAsc(left).concat(pivot, sortByAsc(right));
 }
 
-console.log(isContainNumber(123450, 0));
-// => true
-console.log(isContainNumber(123450, 1));
-// => true
-console.log(isContainNumber(123450, 3));
-// => true
-console.log(isContainNumber(123450, 7));
+console.log(sortByAsc([-2, 9, 5, -3]));
+// => => [-3, -2, 5, 9]
+
+// console.log(isContainNumber(123450, 1));
+// // => true
+// console.log(isContainNumber(123450, 3));
+// // => true
+// console.log(isContainNumber(123450, 7));
 // false
